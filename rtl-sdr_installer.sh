42cd4ec4b3d89f0db2aef525f8b0670f81c25084
Yukiho Kikuchi <bclswl0827@foxmail.com>
2019-02-01 22:57:56 +0800

    Update...

---
+++ rtl-sdr_installer.sh
@@ -0,0 +1,20 @@
+apt install build-essential git libfftw3-dev cmake libusb-1.0-0-dev
+git clone git://git.osmocom.org/rtl-sdr.git
+cd rtl-sdr/
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+make install
+ldconfig
+cd ../..
+bash -c 'echo -e "\n# for RTL-SDR:\nblacklist dvb_usb_rtl28xxu\n" >> /etc/modprobe.d/blacklist.conf'
+update-initramfs -u
+rmmod dvb_usb_rtl28xxu
+git clone https://github.com/simonyiszk/openwebrx.git
+git clone https://github.com/simonyiszk/csdr.git
+cd csdr
+make
+make install
+cd /root
+echo -e "\n\n\n\n\n\nOK, Done.\nAfter that, edit 'openwebrx/config_webrx.py' and run 'python openwebrx.py'.\nEnjoy!"
